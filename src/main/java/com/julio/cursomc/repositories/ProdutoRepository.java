80d4ea82be726cdfe02647d3e322276de4d65897
Julio Cesar <j.juliocesargoncalves@gmail.com>
2021-04-15 19:24:01 -0300

    Associaçao de produtos e categorias

---
+++ src/main/java/com/julio/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,15 @@
+package com.julio.cursomc.repositories;
+
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.julio.cursomc.domain.Categoria;
+import com.julio.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+}
