8b608dbe7002db74543c82cc24369502bc4d97a9
Julio Cesar <j.juliocesargoncalves@gmail.com>
2021-04-15 17:59:29 -0300

    Implementação classe principal para salvar dados banco h2

--- src/main/java/com/julio/cursomc/CursomcApplication.java
+++ src/main/java/com/julio/cursomc/CursomcApplication.java
@@ -1,13 +1,34 @@
 package com.julio.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.julio.cursomc.domain.Categoria;
+import com.julio.cursomc.repositories.CategoriaRepository;
+import com.sun.xml.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
